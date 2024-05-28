1f044e48a2b6915425646b8f9f239dabf87287bf
Aglalberto <aglalfilho@gmail.com>
2024-05-28 20:23:18 -0300

    first commit

---
+++ 01.js
@@ -0,0 +1,10 @@
+let num1 = 29
+let num2 = 30
+
+if(num1 > num2) {
+    console.log(`${num1} é maior que ${num2}`)
+} else if (num1 < 2){
+    console.log(`${num2} é maior que ${num1}`)
+} else {
+    console.log(`${num1} é igual a ${num2}`)
+}
